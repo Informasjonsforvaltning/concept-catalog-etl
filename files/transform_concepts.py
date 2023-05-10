b9d01fbc76d443564f7c09ed15d014cfa868e1d6
valosnah <hans.olav.sund@digdir.no>
2023-05-10 10:08:23 +0200

    chore: sanity

--- files/transform_concepts.py
+++ files/transform_concepts.py
@@ -2,59 +2,57 @@ import json
 import argparse
 import os
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
-old_base_uri = os.environ['OLD_BASE_URI']
-new_base_uri = os.environ['NEW_BASE_URI']
 
 
 def transform(c_file):
     concepts = openfile(c_file)
     transformed_concepts = {}
     transformed_count = 0
-    failed_count = 0
     failed = {}
-    for concept_key in concepts:
-        result = transform_concept(concepts[concept_key])
-        if result["transformed"]:
-            transformed_concepts[concept_key] = result["transformed"]
-            transformed_count += 1
-        if result["not_transformed"]:
-            failed[concept_key] = result["not_transformed"]
-            failed_count += 1
-    print("Total number of transformed concepts: " + str(transformed_count))
-    print("Total number of non-transformed concepts: " + str(failed_count))
-    with open("not_transformed.json", 'w', encoding="utf-8") as err_file:
-        json.dump(failed, err_file, ensure_ascii=False, indent=4)
+    for concept in concepts:
+        result = transform_concept(concept)
+        transformed_concepts[result.get("_id")] = result
+    #        transformed_count += 1
+    # print("Total number of transformed concepts: " + str(transformed_count))
+    # with open("not_transformed.json", 'w', encoding="utf-8") as err_file:
+    #     json.dump(failed, err_file, ensure_ascii=False, indent=4)
     return transformed_concepts
 
 
 def transform_concept(concept):
-    see_also = concept.get("seOgså")
-    see_also = see_also if see_also else []
-    new_see_also = []
-    has_been_modified = False
-    for url in see_also:
-        modified_url = replace_url(url)
-        if modified_url:
-            has_been_modified = True
-            new_see_also.append(modified_url)
-        else:
-            new_see_also.append(url)
-    transformed_concept = {}
-    if has_been_modified:
-        transformed_concept["seOgså"] = new_see_also
-        return {"transformed": transformed_concept}
-    else:
-        return {"not_transformed": concept}
+    # Status er ikke 1-1, vi har enum UTKAST, GODKJENT, HOERING, PUBLISERT
+    # Hva er vår term teknisk begrepsnavn? (tillattTerm? Datastrukturterm?)
 
+    transformed_concept = {
+        "_id": concept.term.get("identifier"),
+        "_class": "no.fdk.concept_catalog.model.Begrep",
+        "anbefaltTerm": {
+            "navn": {
+                "nb": concept.term.properties.get("value"),
+                "en": concept.term.localisedProperties
+                .get("http:\/\/purl.org\/dc\/elements\/1.1\/title", {})
+                .get("English (United Kingdom)", {}).get("value"),
+                "nn": concept.term.localisedProperties
+                .get("http:\/\/purl.org\/dc\/elements\/1.1\/title", {})
+                .get("Norwegian Nynorsk", {}).get("value"),
+            }
+        },
+        "ansvarligVirksomhet": {"_id": "974761076"},
+        "bruksområde": {},
+        "definisjon": {
+            "tekst": {
+                "nb": concept.term.properties
+                .get("http:\/\/purl.org\/dc\/elements\/1.1\/description",{})
+                .get("value")
+            }
+        }
 
-def replace_url(url):
-    if old_base_uri in url:
-        return url.replace(old_base_uri, f'{new_base_uri}/collections')
-    return None
+    }
+
+    return transformed_concept
 
 
 def openfile(file_name):
@@ -62,9 +60,8 @@ def openfile(file_name):
         return json.load(json_file)
 
 
-concepts_file = args.outputdirectory + "mongo_concepts.json"
+concepts_file = args.outputdirectory + "skatt_concepts.json"
 outputfileName = args.outputdirectory + "transformed_concepts.json"
-
 
 with open(outputfileName, 'w', encoding="utf-8") as outfile:
     json.dump(transform(concepts_file), outfile, ensure_ascii=False, indent=4)
