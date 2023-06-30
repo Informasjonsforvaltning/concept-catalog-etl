6199d273d6441ff2e7e50c977a24cf428b988612
valosnah <hans.olav.sund@digdir.no>
2023-06-30 14:06:34 +0200

    chore: wip - test load

--- files/load_concepts.py
+++ files/load_concepts.py
@@ -9,12 +9,13 @@ parser.add_argument('-o', '--outputdirectory', help="the path to the directory o
 args = parser.parse_args()
 connection = MongoClient(
     f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/concept-catalogue?authSource=admin&authMechanism=SCRAM-SHA-1""")
-db = connection.begrep
+db = connection['concept-catalogue']
+errorfileName = args.outputdirectory + "load_errors.json"
 
 with open(args.outputdirectory + 'transformed_concepts.json') as begrep_file:
     transformed_json = json.load(begrep_file)
 
-    total_updated = 0
+    total_inserted = 0
     total_failed = 0
     fail_log = {}
     for mongo_id in transformed_json:
@@ -22,14 +23,13 @@ with open(args.outputdirectory + 'transformed_concepts.json') as begrep_file:
         print("Inserting ID: " + mongo_id)
         insert_result = db.begrep.insert_one(transformed_begrep)
         if insert_result:
-            total_updated += 1
+            total_inserted += 1
             print("Successfully updated: " + mongo_id)
         else:
             total_failed += 1
             print("Update failed: " + mongo_id)
             fail_log[mongo_id] = transformed_json[mongo_id]
-        total_updated += 1
-    print("Total number of concepts inserted: " + str(total_updated))
+    print("Total number of concepts inserted: " + str(total_inserted))
     print("Total number of concept inserts failed: " + str(total_failed))
-    with open("load_errors.json", 'w', encoding="utf-8") as err_file:
+    with open(errorfileName, 'w', encoding="utf-8") as err_file:
         json.dump(fail_log, err_file, ensure_ascii=False, indent=4)
