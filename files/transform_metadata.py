8c0c9bbcf7bc610007a6fa19027cd042a415e286
valosnah <hans.olav.sund@digdir.no>
2023-10-11 12:16:12 +0200

    chore: test etl with complete extract

--- files/transform_metadata.py
+++ files/transform_metadata.py
@@ -10,3 +10,20 @@ args = parser.parse_args()
 #  TODO:
 # Finn _id = concept-catalog (..) mongo_id , og _id = identifikator fra mapping og extract
 # Kopier fdkId og issued fra data.brreg til concept-catalog, og slett data.brreg element etter load
+
+def transform(fdk_meta_file, brreg_meta_file):
+    transformed_meta = {}
+    with open(fdk_meta_file) as fdk_file:
+        fdk_meta = json.load(fdk_file)
+    with open(brreg_meta_file) as brreg_file:
+        brreg_meta = json.load(brreg_file)
+
+    return transformed_meta
+
+outputfileName = args.outputdirectory + "transformed_metadata.json"
+fdk_meta_file = args.outputdirectory + "mongo_fdkMeta.json"
+brreg_meta_file = args.outputdirectory + "mongo_brregMeta.json"
+
+
+with open(outputfileName, 'w', encoding="utf-8") as outfile:
+    json.dump(transform(fdk_meta_file, brreg_meta_file), outfile, ensure_ascii=False, indent=4)
