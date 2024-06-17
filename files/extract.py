98161859937be228fd5d1565347bcc9388a1c1ef
Hans Olav Sund <hans.olav.sund@digdir.no>
2024-06-17 15:23:39 +0200

    chore: extract

--- files/extract.py
+++ files/extract.py
@@ -13,28 +13,65 @@ connection = MongoClient(
     f"""mongodb://{input("Username: ")}:{input("Password: ")}@localhost:27017/concept-catalogue?authSource=admin&authMechanism=SCRAM-SHA-1""")
 db = connection["concept-catalogue"]
 
+# Finn unike ID'er fra internSeOgså, internErstattesAv, internBegrepsRelasjon.relatertBegrep
+# Opprett dict med ID'er, der value er liste over _id som den unike ID'en er hentet fra
+# Gjør nytt uttrekk fra begrep med listen over, opprett ny liste med begrep som er true i erPublisert
+# Hvis den listen ikke er tom, så må vi finne begrepene som inneholder disse ID'ene
 
-def get_affected_concepts(source_list):
-    if source_list is None:
-        return False
-    for source in source_list:
-        if source.get("tekst"):
-            if re.search('^{\"?\'?no\"?\'?: ?\"?\'?(.*)(\"+|\'+)}$', str(source["tekst"])) is not None:
-                return True
-    return False
+# Fil_1 : ID'ene som har blitt publisert
+# Fil_2: Begrep som har et av begrepene i fil_1 som intern relasjon
 
 
-extracted_concepts = list(db.begrep.find({"ansvarligVirksomhet._id": "974761076"}))
+#seOgså
+#erstattesAv
+#begrepsRelasjon (obj)
+
+
+extracted_concepts = list(db.begrep.find())
+intern_relation_concepts = {}
+published_concepts = []
 affected_concepts = {}
+
 for id_dict in extracted_concepts:
-    _id = id_dict["_id"]
-    if get_affected_concepts(
-            id_dict
-            .get("definisjon", {})
-            .get("kildebeskrivelse", {})
-            .get("kilde")):
-        affected_concepts[_id] = {}
-        affected_concepts[_id]["definisjon"] = id_dict["definisjon"]
+    concept_id = id_dict["_id"]
+    internSeeAlso = id_dict.get("internSeOgså", [])
+    internErstattesAv = id_dict.get("internErstattesAv", [])
+    internBegrepsRelasjon = id_dict.get("internBegrepsRelasjon", [])
+
+    for internSeeAlso_id in internSeeAlso:
+        if internSeeAlso_id not in intern_relation_concepts:
+            intern_relation_concepts[internSeeAlso_id] = [concept_id]
+        else:
+            intern_relation_concepts[internSeeAlso_id].append(concept_id)
+
+    for internErstattesAv_id in internErstattesAv:
+        if internErstattesAv_id not in intern_relation_concepts:
+            intern_relation_concepts[internErstattesAv_id] = [concept_id]
+        else:
+            intern_relation_concepts[internErstattesAv_id].append(concept_id)
+
+    for internBegrepsRelasjon_dict in internBegrepsRelasjon:
+        internBegrepsRelasjon_id = internBegrepsRelasjon_dict.get("relatertBegrep")
+        if internBegrepsRelasjon_id is not None:
+            if internBegrepsRelasjon_id not in intern_relation_concepts:
+                intern_relation_concepts[internBegrepsRelasjon_id] = [concept_id]
+            else:
+                intern_relation_concepts[internBegrepsRelasjon_id].append(concept_id)
+
+for id_dict in extracted_concepts:
+    concept_id = id_dict["_id"]
+    isPublished = id_dict.get("erPublisert", False)
+    if concept_id in intern_relation_concepts and isPublished:
+        published_concepts.append(concept_id)
+
+for id_dict in extracted_concepts:
+    concept_id = id_dict["_id"]
+    if concept_id in published_concepts:
+        affected_concepts[concept_id] = id_dict
+
+
+with open(args.outputdirectory + 'published_concepts.json', 'w', encoding="utf-8") as outfile:
+    json.dump(published_concepts, outfile, ensure_ascii=False, indent=4)
 
 with open(args.outputdirectory + 'affected_concepts.json', 'w', encoding="utf-8") as outfile:
     json.dump(affected_concepts, outfile, ensure_ascii=False, indent=4)
